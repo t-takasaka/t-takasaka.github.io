93d7c3464e4c5b128d16fa24fbb8222d1a4db95a
nakamura <nakamura@live2d.com>
2017-12-11 20:28:34 +0900

    Delete functions.

--- live2d-clmtrackr/js/pixiClmtrackr.js
+++ live2d-clmtrackr/js/pixiClmtrackr.js
@@ -136,22 +136,6 @@ function onComplate(loader, resources) {
 	window.onresize = resize;
 }
 
-function supports_video() {
-  return !!document.createElement('video').canPlayType;
-}
-
-function supports_h264_baseline_video() {
-  if (!supports_video()) { return false; }
-  var v = document.createElement("video");
-  return v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-}
-
-function supports_webm_video() {
-  if (!supports_video()) { return false; }
-  var v = document.createElement("video");
-  return v.canPlayType('video/webm; codecs="vp8"');
-}
-
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 window.requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
 window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
